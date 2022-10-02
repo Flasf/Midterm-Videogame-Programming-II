e6e6dc4e79fb592f669261211ed3e90d9991724b
Ghost31Predador <102180862+Ghost31Predador@users.noreply.github.com>
2022-10-02 14:31:25 -0500

    Menu Principal
    El menu principal funciona al 90 % (Falta que funcione el boton de play)

--- Midterm - Time Game/Assets/MenuPrincipalObjects/LogicaFullScreen.cs
+++ Midterm - Time Game/Assets/MenuPrincipalObjects/LogicaFullScreen.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class LogicaFullScreen : MonoBehaviour
 {
     public Toggle toggle;
-    public Dropdown resolucionesDropDown;
+    // public Dropdown resolucionesDropDown;
     Resolution[] resoluciones;
 
     // Start is called before the first frame update
@@ -41,7 +41,7 @@ public class LogicaFullScreen : MonoBehaviour
     public void RevisarResolucion()
     {
         resoluciones = Screen.resolutions;
-        resolucionesDropDown.ClearOptions();
+        //resolucionesDropDown.ClearOptions();
         List<string> opciones = new List<string>();
         int resolucionActual = 0;
 
@@ -56,17 +56,17 @@ public class LogicaFullScreen : MonoBehaviour
                 resolucionActual = i;
             }
         }
-        resolucionesDropDown.AddOptions(opciones);
-        resolucionesDropDown.value = resolucionActual;
-        resolucionesDropDown.RefreshShownValue();
+        //resolucionesDropDown.AddOptions(opciones);
+        //resolucionesDropDown.value = resolucionActual;
+       // resolucionesDropDown.RefreshShownValue();
 
-        resolucionesDropDown.value = PlayerPrefs.GetInt("numeroResolucion", 0);
+        //resolucionesDropDown.value = PlayerPrefs.GetInt("numeroResolucion", 0);
 
     }
 
     public void CambiarResolucion(int indiceResolucion)
     {
-        PlayerPrefs.SetInt("numeroResolucion", resolucionesDropDown.value);
+        //PlayerPrefs.SetInt("numeroResolucion", resolucionesDropDown.value);
 
         Resolution resolucion = resoluciones[indiceResolucion];
         Screen.SetResolution(resolucion.width, resolucion.height, Screen.fullScreen);
