0a1f0e1b82030432b319d9b53e24bfbba31ec0ee
Ghost31Predador <102180862+Ghost31Predador@users.noreply.github.com>
2022-10-07 13:02:06 -0500

    Efecto Parallax
    Se implemento fondo de pantalla y el efecto parallax (funciona a medias pero se puede dejar asi)

--- Midterm - Time Game/Assets/Scripts/Camara/CamaraController.cs
+++ Midterm - Time Game/Assets/Scripts/Camara/CamaraController.cs
@@ -6,14 +6,28 @@ public class CamaraController : MonoBehaviour
 {
     public Transform target;
 
+    public Transform farBackground, middleBackground; 
+
+    private float lastXPos; 
+
+    
     void Start()
     {
-        
+        lastXPos = transform.position.x; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3 (target.position.x, transform.position.y, transform.position.z);
+        transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+
+        float amountToMoveX = transform.position.x - lastXPos;
+
+        farBackground.position = farBackground.position + new Vector3(amountToMoveX, 0f, 0f);
+
+        middleBackground.position += new Vector3(amountToMoveX * .5f, 0f, 0f);
+
+
+        lastXPos = transform.position.x;
     }
 }
