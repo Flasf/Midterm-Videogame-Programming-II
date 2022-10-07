259e34668de5126db1ff8c6f13eba509471390d0
Ghost31Predador <102180862+Ghost31Predador@users.noreply.github.com>
2022-10-07 12:14:29 -0500

    Camara
    Se mejoro la camara y el personaje dejo de saltar

---
+++ Midterm - Time Game/Assets/Scripts/Camara/CamaraController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraController : MonoBehaviour
+{
+    public Transform target;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3 (target.position.x, transform.position.y, transform.position.z);
+    }
+}
