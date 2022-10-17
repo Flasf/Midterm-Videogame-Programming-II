bf235e933f23cf256205368f9fd7059ae05c1e85
Flasf <abdielr507@gmail.com>
2022-10-17 10:49:15 -0500

    Added Dash, updated PlayerMovement to the New Input System

--- Midterm - Time Game/Assets/Scripts/PlayerMovement.cs
+++ Midterm - Time Game/Assets/Scripts/PlayerMovement.cs
@@ -16,11 +16,16 @@ public class PlayerMovement : MonoBehaviour
     private bool isFacingRight = true;
     private float jumpPower = 16;
 
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 24f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
+
+    //Note: serializeFiled let's us access private variables on Unity, yet it does not allow them to be used in other scripts
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
-
-
-
+    [SerializeField] private TrailRenderer trailRenderer;
 
     public LayerMask layerGround;
     public float distRayCast = 0.6f;
@@ -41,14 +46,21 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
+        if (isDashing)//prevents player from moving, jumping and fliping while dashing
+        {
+            return;
+        }
+        //horizontal = Input.GetAxisRaw("Horizontal");
         //CheckIsOnGround();
         //Jump();
 
         Flip();
 
         ChangeAnimationIfActionIsMade();
+        
+        
 
+        /* CODE NOT NEEDED, BUT FUNCTIONAL EITHER WAY
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2 (rb.velocity.x, jumpPower);
@@ -57,7 +69,7 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonUp("Jump") && rb.velocity.y>0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y *0.5f);
-        }
+        }*/
 
         
 
@@ -67,10 +79,17 @@ public class PlayerMovement : MonoBehaviour
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
     private void FixedUpdate()
+    {
+        if(isDashing)
         {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-            //Move();
+            return;
         }
+
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        
+            //Move();
+    }
     
     private void Flip()
     {
@@ -81,6 +100,49 @@ public class PlayerMovement : MonoBehaviour
             localScale.x *= -1f;
             transform.localScale = localScale;
         }
+    }
+
+    //YT: bendux. this guy deserves heaven
+    public void Jump(InputAction.CallbackContext context)//Unity new input system doesn't need to be checked in update or fixed update btw
+    {
+        if (context.performed && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
+        //this one allows us to jump higher by longer pressing  the button
+        if (context.canceled && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+
+    public void ActivateDash(InputAction.CallbackContext context)
+    {
+        if (context.performed && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0; //resets gravity to 0
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);//transform.localScale.x means the direction player is facing
+        trailRenderer.emitting = true;
+        yield return new WaitForSeconds(dashingTime);//this is to prevent player from dashing infinitely
+        trailRenderer.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+
     }
     private void ChangeAnimationIfActionIsMade()
     {
