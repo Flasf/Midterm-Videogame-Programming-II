ff1dbef54b63a00d91828c9061dc546ee7290613
Flasf <abdielr507@gmail.com>
2022-10-17 10:53:21 -0500

    Deleted extra code

--- Midterm - Time Game/Assets/Scripts/PlayerMovement.cs
+++ Midterm - Time Game/Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public InputActionAsset inputAsset;//no
     public Rigidbody2D rb;
-    public float moveSpeed = 120;
-    public float jumpForce = 120;//no
-    public CharacterController2D controller;//no
 
     private float horizontal;
     private float speed = 8f;
@@ -26,10 +22,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private TrailRenderer trailRenderer;
-
-    public LayerMask layerGround;
-    public float distRayCast = 0.6f;
-    public bool isOnGround;
     
 
     private Animator playerAnimator;
@@ -50,9 +42,6 @@ public class PlayerMovement : MonoBehaviour
         {
             return;
         }
-        //horizontal = Input.GetAxisRaw("Horizontal");
-        //CheckIsOnGround();
-        //Jump();
 
         Flip();
 
@@ -60,7 +49,7 @@ public class PlayerMovement : MonoBehaviour
         
         
 
-        /* CODE NOT NEEDED, BUT FUNCTIONAL EITHER WAY
+        /* CODE NOT NEEDED, BUT FUNCTIONAL IN CASE SOMETHING HAPPENS TO NEW INPUT METHOD
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2 (rb.velocity.x, jumpPower);
@@ -86,9 +75,6 @@ public class PlayerMovement : MonoBehaviour
         }
 
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-
-        
-            //Move();
     }
     
     private void Flip()
@@ -170,79 +156,4 @@ public class PlayerMovement : MonoBehaviour
             playerAnimator.SetBool("isFalling", true);
         }
     }
-    
-    /*
-    private void Move()
-    {
-        rb.velocity = new Vector3(GetAxisDirectAsset("Move").x * moveSpeed * Time.unscaledDeltaTime, rb.velocity.y, 0);
-        
-        isMoving = true;
-    }
-    
-    private Vector2 GetAxisDirectAsset(string action)
-    {
-        return inputAsset.FindActionMap("Player").FindAction(action).ReadValue<Vector2>();
-    }
-
-    private void CheckIsOnGround()
-    {
-        isOnGround = Physics2D.Raycast(transform.position, Vector2.down, distRayCast, layerGround.value); //Tira un raycast al suelo para saber si está en el piso o no
-        if (isOnGround && canJump == false)
-        {
-            canJump = true;
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == 6) //layer 6 is ground
-        {
-            canJump = true;
-        }
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer != 6) //layer 6 is ground
-        {
-            canJump = false;
-        }
-    }
-
-    
-    private void Jump()
-    {
-        if (isOnGround)
-        {
-            
-            if (GetKeyDown("Jump") && canJump)
-            {
-                canJump = false;
-                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Force);
-            }
-        }
-    }
-
-    private void OnDrawGizmos() //para mostrar el raycast de CheckIsOnGround
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * distRayCast);   
-    }
-
-    //Parecido al Anterior Input System
-    private bool GetKeyDown(string action)
-    {
-        return inputAsset.FindActionMap("Player").FindAction(action).WasPerformedThisFrame();
-    }
-    
-    private bool GetKey(string action)
-    {
-        return inputAsset.FindActionMap("Player").FindAction(action).IsPressed();
-    }
-
-    private bool GetKeyUp(string action)
-    {
-        return inputAsset.FindActionMap("Player").FindAction(action).WasReleasedThisFrame();
-    }*/
-
-    
 }
