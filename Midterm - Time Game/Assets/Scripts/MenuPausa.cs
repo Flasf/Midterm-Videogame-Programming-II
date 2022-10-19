81b026ab7c253b1b94b013cecddc49e3f07a76e4
Flasf <abdielr507@gmail.com>
2022-10-19 00:18:43 -0500

    Revert "Merge branch 'main' of https://github.com/Flasf/Midterm-Videogame-Programming-II"
    This reverts commit c908843c3c1e3060a696435fd3a9a17888e0445c, reversing
    changes made to 52e80d610a6ed502ec1350727b9d69aacf854173.

--- Midterm - Time Game/Assets/Scripts/MenuPausa.cs
+++ Midterm - Time Game/Assets/Scripts/MenuPausa.cs
@@ -31,7 +31,6 @@ public class MenuPausa : MonoBehaviour
         Time.timeScale = 0f;
         botonPausa.SetActive(false);
         menuPausa.SetActive(true);
-        AudioListener.pause = true;
 
     }
 
@@ -41,7 +40,7 @@ public class MenuPausa : MonoBehaviour
         Time.timeScale = 1f;
         botonPausa.SetActive(true);
         menuPausa.SetActive(false);
-        AudioListener.pause = false;
+
     }
 
     public void MenuPrincipal()
