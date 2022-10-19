c78b9d488f6e8b052e3373149e4b276b9e6a8c48
Flasf <abdielr507@gmail.com>
2022-10-18 20:17:43 -0500

    Made Slowtime work with controller

--- Midterm - Time Game/Assets/Scripts/SlowDown.cs
+++ Midterm - Time Game/Assets/Scripts/SlowDown.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SlowDown : MonoBehaviour
 {
     public GameObject dimBackground;
+
+    private bool canSlowTime = true;
+    private bool isSlowingTime;
+    private float slowingTime_LimitTime = 1f;
+    private float slowingTimeCooldown = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +19,7 @@ public class SlowDown : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    /*void Update()
     {
         if(Input.GetKeyDown(KeyCode.Q))
         {
@@ -24,5 +31,37 @@ public class SlowDown : MonoBehaviour
             Time.timeScale = 1;
             dimBackground.SetActive(false);
         }
+    }*/
+    
+    public void SlowTime(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            canSlowTime = true;
+            Time.timeScale = .2f;
+            dimBackground.SetActive(true);
+            //StartCoroutine(SlowTimeCoroutime(context.duration));
+        }
+        else if (context.canceled)
+        {
+            canSlowTime = false;
+            Time.timeScale = 1;
+            dimBackground.SetActive(false);
+        }
     }
+    /*
+    private IEnumerator SlowTimeCoroutime(double time)
+    {
+        canSlowTime = false;
+        isSlowingTime = true;
+        Time.timeScale = .2f;
+        dimBackground.SetActive(true);
+        yield return new WaitForSeconds((float)time);//this is to prevent player from slowingTime infinitely
+        Time.timeScale = 1;
+        dimBackground.SetActive(false);
+        isSlowingTime = false;
+        yield return new WaitForSeconds(slowingTimeCooldown);
+        canSlowTime = true;
+        //tengo que poner StopCoroutine?
+    }*/
 }
