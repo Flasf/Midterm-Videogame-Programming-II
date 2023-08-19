6b4dfd4bafc6c95827873c090cade2c8ae70784d
Abdiel Baldonedo <abdielr507@gmail.com>
2023-08-19 07:09:09 -0500

    Corrected enemy movement
    now it patrols from 43 to 20 in x position

--- Midterm - Time Game/Assets/Scripts/Enemies/EnemyMovement.cs
+++ Midterm - Time Game/Assets/Scripts/Enemies/EnemyMovement.cs
@@ -8,6 +8,9 @@ public class EnemyMovement : MonoBehaviour
 
     public Rigidbody2D rb;
 
+    private bool facingRight = true;
+
+
     void Start()
     {
         speed = 6;
@@ -16,14 +19,28 @@ public class EnemyMovement : MonoBehaviour
     void Update()
     {
         //This code could be used to make moving platforms hmm
-        if(transform.position.x > -1)
+        if (transform.position.x > 43)
         {
             speed = -6;
+            if (!facingRight) { Flip(); }
         }
-        else if (transform.position.x < -11)
+        else if (transform.position.x < 21)
         {
             speed = 6;
+            if (!facingRight) { Flip(); }
         }
         transform.Translate(Time.deltaTime * speed, 0, 0);
     }
+
+    void Flip()
+    {
+        // Switch the way the player is labelled as facing
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
+
